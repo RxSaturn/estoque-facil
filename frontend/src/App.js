1a5747289d56c6979871176e828c86cda23c206d
Rx Saturn <0050718@academico.ifmg.edu.br>
2025-03-31 01:05:46 -0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,37 +1,37 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 // Importação de componentes
-import Navbar from './components/layout/Navbar';
-import Footer from './components/layout/Footer';
-import PrivateRoute from './components/PrivateRoute';
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
+import PrivateRoute from "./components/PrivateRoute";
 
 // Importação de páginas
-import Login from './pages/Login';
-import Dashboard from './pages/Dashboard';
-import Produtos from './pages/Produtos';
-import AdicionarProduto from './pages/AdicionarProduto';
-import EditarProduto from './pages/EditarProduto';
-import Movimentacao from './pages/Movimentacao';
-import Historico from './pages/Historico';
-import Relatorios from './pages/Relatorios';
-import Usuarios from './pages/Usuarios';
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import Produtos from "./pages/Produtos";
+import AdicionarProduto from "./pages/AdicionarProduto";
+import EditarProduto from "./pages/EditarProduto";
+import Movimentacao from "./pages/Movimentacao";
+import Historico from "./pages/Historico";
+import Relatorios from "./pages/Relatorios";
+import Gerenciamento from "./pages/Gerenciamento";
 
 // Importação de contextos
-import { AuthProvider } from './contexts/AuthContext';
+import { AuthProvider } from "./contexts/AuthContext";
 
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <Router>
       <AuthProvider>
         <div className="app-container">
-          <ToastContainer 
-            position="top-center" 
-            autoClose={3000} 
+          <ToastContainer
+            position="top-center"
+            autoClose={3000}
             hideProgressBar={false}
             newestOnTop={true}
             closeOnClick
@@ -46,41 +46,43 @@ function App() {
             <Routes>
               <Route path="/" element={<Login />} />
               <Route path="/login" element={<Login />} />
-              <Route 
-                path="/dashboard" 
-                element={<PrivateRoute component={Dashboard} />} 
+              <Route
+                path="/dashboard"
+                element={<PrivateRoute component={Dashboard} />}
               />
-              <Route 
-                path="/produtos" 
-                element={<PrivateRoute component={Produtos} />} 
+              <Route
+                path="/produtos"
+                element={<PrivateRoute component={Produtos} />}
               />
-              <Route 
-                path="/produtos/adicionar" 
-                element={<PrivateRoute component={AdicionarProduto} />} 
+              <Route
+                path="/produtos/adicionar"
+                element={<PrivateRoute component={AdicionarProduto} />}
               />
-              <Route 
-                path="/produtos/editar/:id" 
-                element={<PrivateRoute component={EditarProduto} />} 
+              <Route
+                path="/produtos/editar/:id"
+                element={<PrivateRoute component={EditarProduto} />}
               />
-              <Route 
-                path="/movimentacao" 
-                element={<PrivateRoute component={Movimentacao} />} 
+              <Route
+                path="/movimentacao"
+                element={<PrivateRoute component={Movimentacao} />}
               />
               <Route
                 path="/movimentacoes/adicionar"
                 element={<Movimentacao />}
               />
-              <Route 
-                path="/historico" 
-                element={<PrivateRoute component={Historico} />} 
+              <Route
+                path="/historico"
+                element={<PrivateRoute component={Historico} />}
               />
-              <Route 
-                path="/relatorios" 
-                element={<PrivateRoute component={Relatorios} />} 
+              <Route
+                path="/relatorios"
+                element={<PrivateRoute component={Relatorios} />}
               />
-              <Route 
-                path="/usuarios" 
-                element={<PrivateRoute component={Usuarios} adminOnly={true} />} 
+              <Route
+                path="/gerenciamento"
+                element={
+                  <PrivateRoute component={Gerenciamento} adminOnly={true} />
+                }
               />
             </Routes>
           </main>
