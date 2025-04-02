6aa418c0b4cf82d813dd083b5a9ee864fbe7d818
Rx Saturn <0050718@academico.ifmg.edu.br>
2025-04-02 05:00:32 -0300

    Update vendaRoutes.js

--- backend/routes/vendaRoutes.js
+++ backend/routes/vendaRoutes.js
@@ -3,9 +3,12 @@ const router = express.Router();
 const vendaController = require('../controllers/vendaController');
 const { proteger } = require('../middlewares/auth');
 
-// Rotas de vendas
+// Rotas de vendas existentes
 router.post('/', proteger, vendaController.registrarVenda);
 router.get('/', proteger, vendaController.listarVendas);
 router.get('/historico', proteger, vendaController.listarHistorico);
+
+// Nova rota para excluir vendas de produtos removidos
+router.delete('/produtos-removidos', proteger, vendaController.excluirVendasProdutosRemovidos);
 
 module.exports = router;
